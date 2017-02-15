4847fda8cb5d40bd33151e862856dd4e22dfb0d4
Patrick Simpson <p.simpson@etosis.com>
2017-02-15 12:01:04 +0100

    Added license comments

--- src/AcaciaZPushPlugin/AcaciaZPushPlugin/Stubs/OutlookWrappers/ItemsWrapper.cs
+++ src/AcaciaZPushPlugin/AcaciaZPushPlugin/Stubs/OutlookWrappers/ItemsWrapper.cs
@@ -1,4 +1,20 @@
-﻿using Acacia.Utils;
+﻿/// Copyright 2017 Kopano b.v.
+/// 
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Affero General Public License, version 3,
+/// as published by the Free Software Foundation.
+/// 
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+/// GNU Affero General Public License for more details.
+/// 
+/// You should have received a copy of the GNU Affero General Public License
+/// along with this program.If not, see<http://www.gnu.org/licenses/>.
+/// 
+/// Consult LICENSE file for details
+
+using Acacia.Utils;
 using System;
 using System.Collections;
 using System.Collections.Generic;
